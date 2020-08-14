f298ccef2060d01f752579da4235bedc0dd9e8ee
bengeos <george.beng@gmail.com>
2020-08-14 17:16:00 +0300

    updates

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    const error = new Error("Whoops! Unknown Error");
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: { message: error.message }
+    });
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
